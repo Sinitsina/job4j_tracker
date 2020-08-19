24cee368c18cc89042f97dfa5b18234c2da1d40a
Sinitsina <sinitsina.olga.a@gmail.com>
2020-08-19 10:44:00 +0500

    1.1. Тесты на ValidateInput. [#296015]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -83,6 +83,28 @@ public class StartUITest {
                 ));
     }
 
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"8","0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = {
+                new ExitAction()
+        };
+        new StartUI(out).init(in, tracker, actions);
+        assertThat(out.toString(), is(
+                String.format(
+                        "Menu.%n"
+                                + "0. Exit%n"
+                                + "Wrong input, you can select: 0 .. 0%n"
+                                + "Menu.%n"
+                                + "0. Exit%n"
+                )
+        ));
+    }
+
     /*@Test
     public void whenExit() {
         Output out = new StubOutput();
