7ea8faf0ed833c9f0046b449ce393d5b96bbfa9a
Sinitsina <sinitsina.olga.a@gmail.com>
2020-08-31 14:28:40 +0500

    2. Очередь с приоритетом на LinkedList [#295971]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,9 +14,7 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (element.getPriority() >= task.getPriority()) {
-                break;
-            } else {
+            if (element.getPriority() < task.getPriority()) {
                 index++;
             }
         }
