04fa7429913a8ad2a567fa3a43a683ee1196da0c
Sinitsina <sinitsina.olga.a@gmail.com>
2020-09-03 11:54:52 +0500

    1. Организовать сортировку User [#295966]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,38 @@
+package ru.job4j.collection;
+
+import java.util.Objects;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(User o) {
+        if (this.name.equals(o.name)) {
+            return this.age - o.age;
+        }
+        return name.compareTo(o.name);
+    }
+}
