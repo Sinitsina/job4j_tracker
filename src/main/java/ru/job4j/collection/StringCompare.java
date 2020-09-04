b927563de99856a4a874cb2ef44814ee9eae2138
Sinitsina <sinitsina.olga.a@gmail.com>
2020-09-04 16:49:22 +0500

    3. Компаратор для строк [#295964]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -8,7 +8,7 @@ public class StringCompare implements Comparator<String> {
         for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
             int rsl = Character.compare(left.charAt(i), right.charAt(i));
             if (rsl != 0) {
-                return left.charAt(i) - right.charAt(i);
+                return rsl;
             }
         }
         return left.length() - right.length();
