b661e3c3f1e86f2bce1371319895ce43902c8c6c
Sinitsina <sinitsina.olga.a@gmail.com>
2020-09-11 09:39:02 +0500

    0. Stream API [#295831]

---
+++ src/main/java/ru/job4j/lambda/ArrayStream.java
@@ -0,0 +1,20 @@
+package ru.job4j.lambda;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ArrayStream {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = -3; i < 10; i++) {
+            numbers.add(i);
+        }
+
+        List<Integer> positiveNumbers = numbers.stream().filter(
+                number -> number > 0
+        ).collect(Collectors.toList());
+
+    }
+
+}
