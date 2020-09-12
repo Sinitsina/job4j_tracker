ceff8b641cdeb5eee235b8d5d926dfdc5c7ade22
Sinitsina <sinitsina.olga.a@gmail.com>
2020-09-12 13:08:35 +0500

    4. Преобразование List в Map. [#295835]

---
+++ src/main/java/ru/job4j/stream/MapStudents.java
@@ -0,0 +1,20 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapStudents {
+    private List<Student> students = Arrays.asList(
+            new Student("Ivanov", 60),
+            new Student("Petrov", 40),
+            new Student("Sidorov", 90),
+            new Student("Kozlov", 85));
+
+    static Map<String, Student> convert(List<Student> students) {
+        return students.stream().distinct().collect(
+                Collectors.toMap(
+                        e -> e.getSurname(),
+                        e -> e
+                ));
+    }
+}
---
+++ src/test/java/ru/job4j/stream/MapStudentsTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+import java.util.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class MapStudentsTest {
+    @Test
+    public void whenStudentsSortedInClassA() {
+        List<Student> students = Arrays.asList(
+                new Student("Ivanov", 60),
+                new Student("Petrov", 40),
+                new Student("Sidorov", 90),
+                new Student("Kozlov", 85));
+        Map<String, Student> result = MapStudents.convert(students);
+        Map<String, Student> expected = new HashMap<>();
+        expected.put("Ivanov", new Student("Ivanov", 60));
+        expected.put("Petrov", new Student("Petrov", 40));
+        expected.put("Sidorov", new Student("Sidorov", 90));
+        expected.put("Kozlov", new Student("Kozlov", 85));
+        assertThat(result, is(expected));
+    }
+
+}
