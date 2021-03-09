3e86bce07c5f445d025979f96287e7c1e0421e96
Sinitsina <65966649+Sinitsina@users.noreply.github.com>
2021-03-09 09:27:52 +0500

    Контрольные вопросы. [#295962]

---
+++ src/main/java/ru/job4j/exams/FreezeStr.java
@@ -0,0 +1,22 @@
+package ru.job4j.exams;
+
+import java.util.*;
+
+public class FreezeStr {
+
+    public static boolean eq(String left, String right) {
+        Map<Integer, Character> map = new HashMap<>();
+
+        for(int i = 0; i < left.length(); i++) {
+            map.put(i, left.charAt(i));
+        }
+
+        for(int i = 0; i < right.length(); i++) {
+            if (map.containsValue(right.charAt(i))) {
+                map.values().remove(right.charAt(i));
+            }
+        }
+
+        return map.isEmpty();
+    }
+}
